b7e20346331d0fde54bb16f3fb15207af20fb401
dingxiangfei <dingxiangfei@linux-mgy8.site>
2015-10-18 20:53:47 +0800

    Add test generator

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -4,13 +4,15 @@ class QuestionController < ApplicationController
 	def new
 		begin
 			question_type = QuestionType.find_by! name: params[:type]
+			field = Field.find_by! token: params[:topic]
 			question = Question.new
-			question.configuration = params[:configuration],
-			question.question_type_id = question_type_id	# link to question type
+			question.configuration = params[:configuration]
+			question.question_type_id = question_type.id	# link to question type
+			question.field_id = field.id
 			question.save
 			render :json => question
 		rescue
-			render :json => Struct.new(:error).new("Invalid question type")
+			render :json => {:error => "Invalid question type"}
 		end
 	end
 	def get
--- db/migrate/20151004050749_create_fields_table.rb
+++ db/migrate/20151004050749_create_fields_table.rb
@@ -2,6 +2,7 @@ class CreateFieldsTable < ActiveRecord::Migration
   def change
     create_table :fields do |t|
     	t.string :name
+    	t.string :token, unique: true
     end
   end
 end
