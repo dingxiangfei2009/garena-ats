42e4e2fec7c17d3a684e511e6c3d4cd09cdc31ad
anand-sundaram <anand.sundaram@nus.edu.sg>
2015-10-18 12:01:32 +0800

    merge

--- config/routes.rb
+++ config/routes.rb
@@ -5,6 +5,7 @@ Rails.application.routes.draw do
   get '/candidate', :to => 'pages#candidate'
   get '/question', :to => 'pages#question'
   get '/test', :to => 'pages#test'
+  get '/register', :to => 'pages#register'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
