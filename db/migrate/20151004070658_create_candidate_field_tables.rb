67832785673f8609e46b29f086707df7cf335cea
anand-sundaram <anand.sundaram@nus.edu.sg>
2015-10-18 12:15:09 +0800

    remove duplicate candidate-field table

--- db/migrate/20151004070658_create_candidate_field_tables.rb
+++
@@ -1,8 +0,0 @@
-class CreateCandidateFieldTables < ActiveRecord::Migration
-  def change
-    create_table :candidate_fields do |t|
-    end
-    add_foreign_key :candidate_fields, :candidates
-    add_foreign_key :candidate_fields, :fields
-  end
-end
