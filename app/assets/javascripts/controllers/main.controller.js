ede0876f8d18ef9887d7835dadb7cfc14dd75518
anand-sundaram <anand.sundaram@nus.edu.sg>
2015-10-25 21:14:47 +0800

    made modifications to question setting

--- app/assets/javascripts/controllers/main.controller.js
+++ app/assets/javascripts/controllers/main.controller.js
@@ -11,7 +11,7 @@ app.controller('SidebarController', function($scope) {
         $scope.state = !$scope.state;
     };
     $scope.clickAlert = function(idName) {
-      alert("You clicked me " + idName);
+      // alert("You clicked me " + idName);
       $('#' + idName).addClass('active');
       $('#' + $scope.activeId).removeClass('active');
       $scope.activeId = idName;
@@ -19,62 +19,85 @@ app.controller('SidebarController', function($scope) {
 
 });
 
-app.controller('QuestionController', function($scope) {
+app.controller('QuestionController', ['$scope', '$http', function($scope, $http) {
 
     $scope.state = false;
-    $scope.typeState = 'DEF';
+    $scope.typeState = [0];
+    $scope.questionType = [];
+    $scope.questionList = [1];
+    $scope.difficulty = [];
+    $scope.question = [];
+    $scope.answer = [];
+
+    $scope.questionObject = {
+      topic: {
+        name: 'Select Topic',
+        value: '0'
+      },
+      type: {
+        name: 'Select Question Type',
+        value: '0'
+      },
+      questionText: '',
+      difficulty: {
+        name: 'Select Difficulty Level',
+        value: '0'
+      },
+      answer: {}
+    };
+    $scope.questions = [$scope.questionObject];
 
     $scope.topics = [
       {
         name: 'Algorithm and Data Structures',
-        value: 'ADS'
+        value: '1'
       },
       {
         name: 'Android',
-        value: 'AND'
+        value: '2'
       },
       {
         name: 'iOS',
-        value: 'IOS'
+        value: '3'
       },
       {
         name: 'Networks',
-        value: 'NET'
+        value: '4'
       },
       {
         name: 'Operating Systems',
-        value: 'OPS'
+        value: '5'
       },
       {
         name: 'Security',
-        value: 'SEC'
+        value: '6'
       },
       {
         name: 'Web Development',
-        value: 'WEB'
+        value: '7'
       }
     ];
 
     $scope.types = [
       {
         name: 'Multiple Choice',
-        value: 'MCQ'
+        value: '1'
       },
       {
         name: 'Multiple Answer',
-        value: 'MAS'
-      },
-      {
-        name: 'Fill in the Blanks',
-        value: 'FIB'
+        value: '2'
       },
       {
         name: 'Subjective Text',
-        value: 'SBT'
+        value: '3'
       },
       {
         name: 'Subjective Code',
-        value: 'SBC'
+        value: '4'
+      },
+      {
+        name: 'Fill in the Blanks',
+        value: '5'
       }
     ];
 
@@ -97,13 +120,78 @@ app.controller('QuestionController', function($scope) {
         $scope.state = !$scope.state;
     };
     $scope.clickAlert = function() {
-      alert("You clicked me!");
+      // alert("You clicked me!");
     }
-    $scope.typeChange = function(typeValue) {
-      $scope.typeState = typeValue;
+    $scope.typeChange = function(type, index) {
+      // $scope.typeState[index] = type.value;
+      // $scope.questionType[index] = type.name;
+      $scope.questions[index].type = type;
+    }
+    $scope.difficultyChange = function(difficulty, index) {
+      $scope.questions[index].difficulty = difficulty;
+    }
+    $scope.topicChange = function(topic, index) {
+      $scope.questions[index].topic = topic;
+    }
+    $scope.addQuestion = function() {
+      // alert("Current length is " + $scope.questionList.length + " new length is " + ($scope.questionList.length + 1));
+      // $scope.questionList.push($scope.questionList.length + 1);
+      // $scope.typeState.push('DEF');
+      $scope.questions.push({
+        topic: {
+          name: 'Select Topic',
+          value: '0'
+        },
+        type: {
+          name: 'Select Question Type',
+          value: '0'
+        },
+        questionText: '',
+        difficulty: {
+          name: 'Select Difficulty Level',
+          value: '0'
+        },
+        answer: {}
+      });
+    }
+    $scope.display = function(val) {
+      alert(val);
+    }
+    $scope.remove = function(index) {
+      // $scope.questionList.splice(index, 1);
+      // $scope.question.splice(index, 1);
+      // $scope.answer.splice(index, 1);
+      // $scope.difficulty.splice(index, 1);
+      // $scope.typeState.splice(index, 1);
+      // $scope.questionType.splice(index, 1);
+      $scope.questions.splice(index, 1);
+    }
+    $scope.submit = function() {
+      for(question in $scope.questions){
+        alert(JSON.stringify(question));
+        $http({
+          method: 'POST',
+          url: '/question/new',
+          data: {
+            type: question.type,
+            topic: question.topic,
+            difficulty: question.difficulty,
+            configuration: {
+              questionText: question.questionText,
+              answer: question.answer
+            }
+          }
+        }).then(function successCallback(response) {
+            // this callback will be called asynchronously
+            // when the response is available
+          }, function errorCallback(response) {
+            // called asynchronously if an error occurs
+            // or server returns response with an error status.
+          });
+      }
     }
 
-});
+}]);
 
 app.controller('TestsController', function($scope) {
   $scope.tests = [
@@ -114,7 +202,7 @@ app.controller('TestsController', function($scope) {
     },
     {
       position: 'Web Developer',
-      candidate: 'Invite',
+      candidate: 'Qunfeng Ye',
       complete: 'Not Completed'
     },
     {
@@ -123,11 +211,43 @@ app.controller('TestsController', function($scope) {
       complete: 'View Score'
     },
   ];
+
+  $scope.positions = [
+    {
+      name: 'Android Developer',
+      value: 'AND'
+    },
+    {
+      name: 'Database Scientist',
+      value: 'DAT'
+    },
+    {
+      name: 'Security Engineer',
+      value: 'SEC'
+    },
+    {
+      name: 'System Admin',
+      value: 'SYS'
+    },
+    {
+      name: 'Web Developer',
+      value: 'WEB'
+    }
+  ];
+
+  $('.ui.selection.dropdown')
+    .dropdown()
+  ;
+  $('.ui.modal')
+    .modal('attach events', '.green.button', 'show')
+  ;
+
+
 });
 
 app.controller('TestController', function($scope) {
   $scope.testName = 'Garena Android Developer Test';
-  
+
   $scope.questions = [
     {
       type: 'MCQ',
@@ -155,12 +275,30 @@ app.controller('TestController', function($scope) {
     }
   ];
 
-  $scope.check = function() {
-    alert("you checked me out");
-    $('.ui.checkbox')
-      .checkbox()
-    ;
+  $scope.aceLoaded = function(_editor) {
+    // Options
+
   };
+
+  $scope.aceChanged = function(e) {
+    //
+    console.log('Ace editor changed');
+    // Get Current Value
+    var currentValue = _editor.getSession().getValue();
+    console.log(currentValue);
+  };
+
+  $scope.check = function() {
+    // alert("you checked me out");
+  };
+
+  $('#attempt').progress({
+    value: 1,
+    total: $scope.questions.length,
+    text: {
+      ratio: '{value} of {total}'
+    }
+  });
 
 });
 
@@ -168,6 +306,9 @@ app.directive('multipleChoice', function() {
   return {
       restrict: 'AE',
       replace: 'true',
+      scope: {
+        data: '='
+      },
       templateUrl: '/templates/multiple-choice.html'
   };
 });
@@ -176,6 +317,10 @@ app.directive('multipleAnswer', function() {
   return {
       restrict: 'AE',
       replace: 'true',
+      scope: {
+        'data': '=',
+        'display': '='
+      },
       templateUrl: '/templates/multiple-answer.html'
   };
 });
@@ -184,6 +329,9 @@ app.directive('subjectiveText', function() {
   return {
       restrict: 'AE',
       replace: 'true',
+      scope: {
+        data: '='
+      },
       templateUrl: '/templates/subjective-text.html'
   };
 });
@@ -192,6 +340,9 @@ app.directive('subjectiveCode', function() {
   return {
       restrict: 'AE',
       replace: 'true',
+      scope: {
+        data: '='
+      },
       templateUrl: '/templates/subjective-code.html'
   };
 });
