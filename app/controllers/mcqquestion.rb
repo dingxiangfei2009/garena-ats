01814b0b4d5b3a95b6bd9a9ed80b02d4299876c1
dingxiangfei <dingxiangfei@linux-mgy8.site>
2015-10-28 22:26:12 +0800

    better statistics collection

--- app/controllers/mcqquestion.rb
+++ app/controllers/mcqquestion.rb
@@ -6,8 +6,9 @@ class MCQQuestion {
 	self.token = 'mcq'
 	def initialize(question)
 		config = JSON.parse question.config
+		@id = question.id
 		@description = config['description']
-		@choices = config['choices']
+		@choices = config['answer']
 		@mark_scheme = config['mark_scheme']
 		@mark_scheme = []	# TODO flexible marking scheme
 		@mark = question.mark
@@ -16,13 +17,31 @@ class MCQQuestion {
 	def render(question_config)
 		question_options = question_config['options']
 	end
-	def mark(question_config, answer)
+	def mark(test_response)
 		count = 0;
+		answer = JSON.parse test_response.answer
 		answer_options = answer['options']
+		QuestionStatistic.destroy_all(
+			question_id: @id,
+			test_response_id: test_response.id)
 		answer_options.each do |answer_option|
 			if @choices[answer_option]['correct']
 				count += 1
 			end
+			question_statistic_tag = 'mcq:' + answer_option + ':chosen'
+			question_statistic =
+				QuestionStatistic.find_by(
+					question_id: @id,
+					test_response_id: test_response.id,
+					tag: question_statistic_tag)
+			if !question_statistic
+				question_statistic = QuestionStatistic.new
+				question_statistic.question_id = @id
+				question_statistic.test_response_id = test_response.id
+				question_statistic.tag = question_statistic_tag
+			end
+			question_statistic.value = 1
+			question_statistic.save
 		end
 		correct_total = @choices.count { |choice| choice['correct'] }
 		# TODO flexible marking scheme
@@ -30,32 +49,40 @@ class MCQQuestion {
 		@mark_scheme[correct_total][correct_total] = mark;
 		@mark_scheme[correct_total][count]	# return
 	end
-	def get_statistics(question, options)
+	def statistics(question, options)
 		counts = Hash.new
-		statistics = question.question_statistic
-		if !statistics
-			statistics = QuestionStatistic.new
-			statistics.question_id = question.id
+		@choices.each do |choice, key|
+			count = QuestionStatistic.where(
+				question_id: @id,
+				tag: 'mcq:' + choice + ':chosen')
+				.sum(:value)
+			counts[key] = count
 		end
+		counts
+		# statistics = question.question_statistic
+		# if !statistics
+		# 	statistics = QuestionStatistic.new
+		# 	statistics.question_id = question.id
+		# end
 		# we only count backward to a given timespan
-		options[:timespan] = options[:timespan] or DateTime.now
-		responses = Question.joins(:test_responses).select('answer').where(
-			test_responses: {
-				id: question.id
-				})
-			.where('test_responses.updated_at > ?', options[:timespan]);
-		responses.each do |response|
-			answer = JSON.parse response.answer
-			answer.each do |answer_option|
-				if counts[answer_option]
-					counts[answer_option] += 1
-				else
-					counts[answer_option] = 1
-				end
-			end
-		end
-		statistics.data = JSON.generate counts
-		statistics.save
-		statistics
+		# options[:timespan] = options[:timespan] or DateTime.now
+		# responses = Question.joins(:test_responses).select('answer').where(
+		# 	test_responses: {
+		# 		id: question.id
+		# 		})
+		# 	.where('test_responses.updated_at > ?', options[:timespan]);
+		# responses.each do |response|
+		# 	answer = JSON.parse response.answer
+		# 	answer.each do |answer_option|
+		# 		if counts[answer_option]
+		# 			counts[answer_option] += 1
+		# 		else
+		# 			counts[answer_option] = 1
+		# 		end
+		# 	end
+		# end
+		# statistics.data = JSON.generate counts
+		# statistics.save
+		# statistics
 	end
 }
