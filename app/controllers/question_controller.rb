30f2d4e9cd5e173dbb14ef39f918861615ade63b
dingxiangfei <dingxiangfei@linux-mgy8.site>
2015-11-06 21:23:20 +0800

    add query for questions

--- app/controllers/question_controller.rb
+++ app/controllers/question_controller.rb
@@ -83,4 +83,27 @@ class QuestionController < ApplicationController
 		end
     render json: query.offset(params[:offset] || 0).limit(@@LIMIT)
 	end
+	def list
+		if params[:disabled]
+			query = Question.where(enabled: false)
+		else
+			query = Question.where(enabled: true)
+		end
+
+		if params[:topic]
+			topic = Field.find_by! token: params[:topic]
+			query = query.where(field_id: topic.id)
+		end
+
+		if params[:difficulty]
+			query = query.where(difficulty: params[:difficulty])
+		end
+
+		if params[:type]
+			type = QuestionType.find_by! name: params[:type]
+			query = query.where(question_type_id: type.id)
+		end
+
+		render json: query.limit(@@LIMIT)
+	end
 end
