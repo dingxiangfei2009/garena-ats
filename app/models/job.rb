8acb10a62772b566b6a2e152620faa4b71f91b1a
dingxiangfei <dingxiangfei@linux-mgy8.site>
2015-11-13 00:23:33 +0800

    Add evaluation page
    Anand: ask me for the missing javascript files, such as requirejs and vis-compat

--- app/models/job.rb
+++ app/models/job.rb
@@ -2,5 +2,5 @@ class Job < ActiveRecord::Base
 	self.table_name = 'jobs'
 	has_many :fields, through: :job_fields
 	has_one :job_test_parameter
-	belongs_to :application
+	has_many :applications
 end
