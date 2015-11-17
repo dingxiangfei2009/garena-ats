d4afff27792eeead09dbb06dc9e33323d1f72c05
dingxiangfei <dingxiangfei@linux-mgy8.site>
2015-11-17 23:07:56 +0800

    registration form is complete

--- app/assets/javascripts/controllers/register.controller.js
+++ app/assets/javascripts/controllers/register.controller.js
@@ -24,19 +24,33 @@ angular.module('app').controller('RegisterController', ['$scope', '$http', '$sce
   });
 
   $scope.setNewPosition = function(pos) {
-    $scope.position = pos.name;
+    $scope.position = pos.value;
   };
 
   $scope.submit = function (){
-    alert(JSON.stringify($scope.firstName + ' ' +
-      $scope.lastName + ' ' +
-      $scope.mobileNo + ' ' +
-      $scope.email + ' ' +
-      $scope.position + ' ' +
-      $scope.githubId + ' ' +
-      $scope.skills + ' '
-    ));
-    $('.ui.modal').modal('show');
+    console.log($scope.skills);
+    $.ajax({
+      url: '/applicants',
+      method: 'POST',
+      data: {
+        email: $scope.email,
+        name: $scope.firstName + ' ' + $scope.lastName,
+        first_name: $scope.firstName,
+        last_name: $scope.lastName,
+        fields: JSON.stringify($scope.skills),
+        description: JSON.stringify({
+          github_id: $scope.githubId,
+          mobile_no: $scope.mobileNo
+        })
+      }
+    }).then(function(data) {
+      return $.ajax({
+        url: `/applicants/${data.id}/job/${$scope.position}`,
+        method: 'POST'
+      });
+    }).done(function() {
+      $('.ui.modal').modal('show');
+    });
   }
 
 }]);
