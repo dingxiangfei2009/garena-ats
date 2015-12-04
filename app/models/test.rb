25402d73fb1adba5573223a0c0e6aaff2c2c81f8
dingxiangfei <dingxiangfei@fedora.Gnossafkom.komr>
2015-12-04 19:27:04 +0800

    random test id; arrange question by topics and types

--- app/models/test.rb
+++ app/models/test.rb
@@ -6,11 +6,11 @@ class Test < ActiveRecord::Base
 	has_many :test_responses
 	has_many :questions, through: :test_responses
 	before_create :random_id
-	
+
 	private
 	def random_id
 		begin
 			self.id = SecureRandom.random_number(2147483648)
-		end while Model.where(id: self.id).exists?
+		end while Test.where(id: self.id).exists?
 	end
 end
