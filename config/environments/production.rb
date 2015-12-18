34ace27258ab6279587b7506eadd824bc970259b
dingxiangfei <dingxiangfei@fedora.Gnossafkom.komr>
2015-12-18 14:19:05 +0800

    switch order

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,10 @@ Rails.application.configure do
   # config.assets.js_compressor = :uglifier
   # config.assets.css_compressor = :sass
 
+  config.assets.enabled = false
+  # TODO: Maintainer, assets pipeline is not friendly with dynamically loaded
+  # javascripts.
+
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
 
