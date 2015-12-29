894b5b856fe792a0a4b868d933387577fb755c7b
dingxiangfei <dingxiangfei@fedora.Gnossafkom.komr>
2015-12-30 01:03:10 +0800

    merge

--- db/migrate/20151004053349_create_mcq_responses_table.rb
+++
@@ -1,9 +0,0 @@
-class CreateMcqResponsesTable < ActiveRecord::Migration
-  def change
-    create_table :mcq_responses do |t|
-    	t.integer :answer
-    end
-    add_foreign_key :mcq_responses, :mcq_tests
-    add_foreign_key :mcq_responses, :mcq_questions
-  end
-end
